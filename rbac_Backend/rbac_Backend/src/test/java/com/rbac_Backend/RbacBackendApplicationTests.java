b84442292b58a4ce54fbd46848b034739e8a9b77
Sham1718 <119720894+Sham1718@users.noreply.github.com>
2026-01-29 12:24:56 +0530

    Started rbac system

---
+++ rbac_Backend/rbac_Backend/src/test/java/com/rbac_Backend/RbacBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbac_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbacBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
